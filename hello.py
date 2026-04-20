0a67a6c9c08268f84a03ad7871355bb70d053b93
bibibobo479 <avey8431@gmail.com>
2026-04-20 11:35:11 +0300

    комит с vs code

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ first_name = input("Введите ваше имя: ")
 last_name = input("Введите вашу фамилию: ")
 
 # Вывод результата
+print(f"Комит с vs code")
 print(f"\nПривет, {first_name} {last_name}!")
-print(f"Ваше имя:2 {first_name}")
+print(f"Ваше имя: {first_name}")
 print(f"Ваша фамилия: {last_name}")
