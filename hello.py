2c86e3345b53de2d320ee686b572e6930c3772fe
bibibobo479 <avey8431@gmail.com>
2026-04-20 05:08:28 +0300

    изменил рпограмму на ввод-вывод фамилии имени

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Hello Python from VS Code on Linux!")
+# Чтение имени и фамилии
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+
+# Вывод результата
+print(f"\nПривет, {first_name} {last_name}!")
+print(f"Ваше имя: {first_name}")
+print(f"Ваша фамилия: {last_name}")
